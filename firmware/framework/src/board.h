21cd718030e5e662cf44dcfdf83e881e6ea0a270
StefanTesmanovic <tesmanovic166@gmail.com>
2024-02-13 16:03:15 +0100

    odradjeno kretanje

--- firmware/framework/src/board.h
+++ firmware/framework/src/board.h
@@ -9,11 +9,18 @@ namespace tetrics_module
         void moveLeft();
         void moveRight();
         void moveDown();
+        void makeNewShape();
         int getTile(int x, int y);
         const int width=10;
         const int height=22;
         int board[10][22];
-
     private:
+        void createShape();
+        int shapeIndex;
+        int currentShape[4][4];
+        int currentShapeX;
+        int currentShapeY;
+        int currentShapeColor;
+
     };
 }
