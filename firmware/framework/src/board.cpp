dbdb3e55c5b7617418fd9ea60fc53b280c227e6d
StefanTesmanovic <tesmanovic166@gmail.com>
2024-02-15 00:07:51 +0100

    operation neutralise red, take n

--- firmware/framework/src/board.cpp
+++ firmware/framework/src/board.cpp
@@ -144,82 +144,51 @@ namespace tetrics_module
 	void board::moveRight()
 	{
 		for (int i = currentShapeX; i < currentShapeX + 4; i++)
-		{
 			for (int j = currentShapeY; j < currentShapeY + 4; j++)
-			{
 				if (board[i][j] < 0 && (i + 1 >= width || board[i + 1][j] > 0))
-				{
 					return;
-				}
-			}
-		}
 
 		for (int i = currentShapeX + 3; i >= currentShapeX; i--)
-		{
 			for (int j = currentShapeY; j < currentShapeY + 4; j++)
-			{
 				if (board[i][j] < 0 && i+1<width) 
 				{
 					board[i + 1][j] = board[i][j];
 					board[i][j] = 0;
 				}
-			}
-		}
+
 		currentShapeX += 1;
 	}
 	void board::moveLeft()
 	{
 		for (int i = currentShapeX; i < currentShapeX + 4; i++)
-		{
 			for (int j = currentShapeY; j < currentShapeY + 4; j++)
-			{
 				if (board[i][j] < 0 && (i <= 0 || board[i - 1][j] > 0))
-				{
 					return;
-				}
-			}
-		}
+				
 		for (int i = currentShapeX; i < currentShapeX + 4; i++)
-		{
 			for (int j = currentShapeY; j < currentShapeY + 4; j++)
-			{
-				if (board[i][j] < 0)
-				{
+				if (i < width && board[i][j] < 0) {
 					board[i - 1][j] = board[i][j];
 					board[i][j] = 0;
 				}
-			}
-		}
 		currentShapeX -= 1;
 	}
 	void board::moveDown()
 	{
 		bool canMove = true;
 		for (int i = currentShapeX; i < currentShapeX + 4; i++)
-		{
 			for (int j = currentShapeY; j < currentShapeY + 4; j++)
-			{
 				if (board[i][j] < 0 && (j + 1 >= height || board[i][j + 1] > 0))
-				{
-					canMove = false;
-					goto izadji;
-				}
-			}
-		}
-	izadji:
-		if (!canMove)
-			return;
+					return;
+
 		for (int j = currentShapeY + 3; j >= currentShapeY; j--)
-		{
 			for (int i = currentShapeX; i < currentShapeX + 4; i++)
-			{
-				if (board[i][j] < 0)
+				if (i < width && board[i][j] < 0)
 				{
 					board[i][j + 1] = board[i][j];
 					board[i][j] = 0;
 				}
-			}
-		}
+			
 		currentShapeY += 1;
 	}
 	bool board::checkCollision()
