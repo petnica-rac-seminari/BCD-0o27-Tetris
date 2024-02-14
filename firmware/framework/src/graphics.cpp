f64f27101739429ab4ca06a19a2927af9f51c86b
unknown <queenkaca196@gmail.com>
2024-02-14 10:27:49 +0100

    a

--- firmware/framework/src/graphics.cpp
+++ firmware/framework/src/graphics.cpp
@@ -2,15 +2,15 @@
 
 void Main::drawStartScreen()
 {
-    
+    // draw::image("start_screen.png", 0, 0);
 }
 
 void Main::drawGameScreen()
 {
-
+    // draw::
 }
 
 void Main::drawEndScreen()
 {
-
+    // draw::image("end_screen.png", 0, 0);
 }
