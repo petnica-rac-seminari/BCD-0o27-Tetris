e81192f55e5a9f49811695cb624bc3c9f16d0a27
Katarina <queenkaca196@gmail.com>
2024-02-14 16:02:56 +0100

    dodate boje

--- firmware/framework/src/gameStates.cpp
+++ firmware/framework/src/gameStates.cpp
@@ -4,55 +4,32 @@ Main::GameState Main::runStartScreen()
 {
 	GameState exitState = GameState::Running;
 
-	const char *start_text = "Start\r\n";
-	srect16 start_text_rect = textFont.measure_text((ssize16)lcd.dimensions(), start_text).bounds().center((srect16)lcd.bounds());
-	const char *exit_text = "Exit\r\n";
-	srect16 exit_text_rect = textFont.measure_text((ssize16)lcd.dimensions(), exit_text).bounds().center(start_text_rect).offset(0, start_text_rect.height());
+	const char *start_text = "Start";
+	srect16 start_text_rect = textFont.measure_text((ssize16)lcd.dimensions(), start_text).bounds().center((srect16)lcd.bounds()).offset(0, -3);
+	const char *exit_text = "Exit";
+	srect16 exit_text_rect = textFont.measure_text((ssize16)lcd.dimensions(), exit_text).bounds().center(start_text_rect).offset(0, 2);
 
-<<<<<<< HEAD
-	int selectedButton = 0;
-	auto renderScene = [&]()
-	{
-		draw::filled_ellipse(lcd, rect16(point16(10, 10), lcd.dimensions().inflate(-20, -20)), color<pixel_type>::red);
-
-		drawJPEG("/a.jpeg", point16(0, 0));
-
-		draw::text(lcd, start_text_rect, start_text, textFont, color<pixel_type>::white);
-		draw::text(lcd, exit_text_rect, exit_text, textFont, color<pixel_type>::white);
-
-		switch (selectedButton)
-		{
-		case 0:
-		{
-			point16 pos = point16(start_text_rect.x1, (start_text_rect.y1 + start_text_rect.y2) / 2).offset(-20, -5);
-			draw::filled_ellipse(lcd, rect16(pos, size16(6, 6)), color<pixel_type>::white);
-=======
-	const char* start_text = "Start";
-    srect16 start_text_rect = textFont.measure_text((ssize16)lcd.dimensions(), start_text).bounds().center((srect16)lcd.bounds()).offset(0, -3);
-	const char* exit_text = "Exit";
-    srect16 exit_text_rect = textFont.measure_text((ssize16)lcd.dimensions(), exit_text).bounds().center(start_text_rect).offset(0, 2);
-	
-	//draw::filled_ellipse(lcd, rect16(point16(10, 10), lcd.dimensions().inflate(-20, -20)), color<pixel_type>::red);	
-	//drawJPEG("/a.jpeg", point16(0, 0));
+	// draw::filled_ellipse(lcd, rect16(point16(10, 10), lcd.dimensions().inflate(-20, -20)), color<pixel_type>::red);
+	// drawJPEG("/a.jpeg", point16(0, 0));
 
 	lcd.clear(lcd.bounds());
 
-	draw::filled_rectangle(lcd, rect16(point16(45, 46),size16(70, 36)), color<pixel_type>::black);
+	draw::filled_rectangle(lcd, rect16(point16(45, 46), size16(70, 36)), color<pixel_type>::black);
 	draw::rectangle(lcd, rect16(point16(45, 46), size16(70, 36)), color<pixel_type>::white);
 	draw::text(lcd, start_text_rect, start_text, textFont, color<pixel_type>::white);
-    draw::text(lcd, exit_text_rect, exit_text, textFont, color<pixel_type>::white);
+	draw::text(lcd, exit_text_rect, exit_text, textFont, color<pixel_type>::white);
 
 	int selectedButton = 0;
-	auto renderScene = [&]() 
+	auto renderScene = [&]()
 	{
 		rect16 startDot = rect16(point16(start_text_rect.left(), (start_text_rect.y1 + start_text_rect.y2) / 2).offset(-10, -6), size16(6, 6));
 		rect16 exitDot = rect16(point16(exit_text_rect.left(), (exit_text_rect.y1 + exit_text_rect.y2) / 2).offset(-10, -6), size16(6, 6));
 		switch (selectedButton)
 		{
-		case 0: {
+		case 0:
+		{
 			lcd.clear(exitDot);
 			draw::filled_ellipse(lcd, startDot, color<pixel_type>::white);
->>>>>>> 9f34c6b0729a1f9788c3cc327d59276bd1e9170b
 			break;
 		}
 		case 1:
@@ -107,10 +84,10 @@ Main::GameState Main::runStartScreen()
 
 Main::GameState Main::runGameScreen()
 {
-	const char* TETRIS_text = "TETRIS";
-    srect16 TETRIS_text_rect = textFont.measure_text((ssize16)lcd.dimensions(), TETRIS_text).bounds().center_horizontal((srect16)lcd.bounds());
-	const char* exitInfo_text = "Click <BACK> to exit game";
-    srect16 exitInfo_text_rect = textFont.measure_text((ssize16)lcd.dimensions(), exitInfo_text).bounds();
+	const char *TETRIS_text = "TETRIS";
+	srect16 TETRIS_text_rect = textFont.measure_text((ssize16)lcd.dimensions(), TETRIS_text).bounds().center_horizontal((srect16)lcd.bounds());
+	const char *exitInfo_text = "Click <BACK> to exit game";
+	srect16 exitInfo_text_rect = textFont.measure_text((ssize16)lcd.dimensions(), exitInfo_text).bounds();
 	exitInfo_text_rect.y1 = 120;
 	exitInfo_text_rect.y2 = 128;
 
@@ -119,7 +96,6 @@ Main::GameState Main::runGameScreen()
 
 	while (true)
 	{
-<<<<<<< HEAD
 		// cistimo ekran
 		lcd.clear(lcd.bounds());
 
@@ -149,19 +125,19 @@ Main::GameState Main::runGameScreen()
 						rectColor = color<pixel_type>::red;
 						break;
 					case 2:
-						rectColor = color<pixel_type>::black;
+						rectColor = color<pixel_type>::orange;
 						break;
 					case 3:
-						rectColor = color<pixel_type>::black;
+						rectColor = color<pixel_type>::yellow;
 						break;
 					case 4:
-						rectColor = color<pixel_type>::black;
+						rectColor = color<pixel_type>::green;
 						break;
 					case 5:
-						rectColor = color<pixel_type>::black;
+						rectColor = color<pixel_type>::blue;
 						break;
 					case 6:
-						rectColor = color<pixel_type>::black;
+						rectColor = color<pixel_type>::violet;
 						break;
 					default:
 						rectColor = color<pixel_type>::black;
@@ -174,10 +150,8 @@ Main::GameState Main::runGameScreen()
 				draw::filled_rectangle(lcd, rectangle, rectColor);
 			}
 		}
-=======
 		TickType_t tick = xTaskGetTickCount();
 		board.frame(tick);
->>>>>>> 9f34c6b0729a1f9788c3cc327d59276bd1e9170b
 	}
 
 	return GameState::Start;
@@ -185,29 +159,30 @@ Main::GameState Main::runGameScreen()
 
 Main::GameState Main::runEndScreen()
 {
-	GameState exitState = GameState::Running;		
+	GameState exitState = GameState::Running;
 
-	const char* play_again_text = "Play again\r\n";
-    srect16 play_again_text_rect = textFont.measure_text((ssize16)lcd.dimensions(), play_again_text).bounds().center((srect16)lcd.bounds()).offset(0, -3);
-	const char* exit_text = "Exit\r\n";
-    srect16 exit_text_rect = textFont.measure_text((ssize16)lcd.dimensions(), exit_text).bounds().center(play_again_text_rect).offset(0, 12);
-	
-	draw::filled_ellipse(lcd, rect16(point16(10, 10), lcd.dimensions().inflate(-20, -20)), color<pixel_type>::red);	
-	//drawJPEG("/a.jpeg", point16(0, 0));
+	const char *play_again_text = "Play again\r\n";
+	srect16 play_again_text_rect = textFont.measure_text((ssize16)lcd.dimensions(), play_again_text).bounds().center((srect16)lcd.bounds()).offset(0, -3);
+	const char *exit_text = "Exit\r\n";
+	srect16 exit_text_rect = textFont.measure_text((ssize16)lcd.dimensions(), exit_text).bounds().center(play_again_text_rect).offset(0, 12);
 
-	draw::filled_rectangle(lcd, rect16(point16(45, 46),size16(70, 36)), color<pixel_type>::black);
+	draw::filled_ellipse(lcd, rect16(point16(10, 10), lcd.dimensions().inflate(-20, -20)), color<pixel_type>::red);
+	// drawJPEG("/a.jpeg", point16(0, 0));
+
+	draw::filled_rectangle(lcd, rect16(point16(45, 46), size16(70, 36)), color<pixel_type>::black);
 	draw::rectangle(lcd, rect16(point16(45, 46), size16(70, 36)), color<pixel_type>::white);
 	draw::text(lcd, play_again_text_rect, play_again_text, textFont, color<pixel_type>::white);
-    draw::text(lcd, exit_text_rect, exit_text, textFont, color<pixel_type>::white);
+	draw::text(lcd, exit_text_rect, exit_text, textFont, color<pixel_type>::white);
 
 	int selectedButton = 0;
-	auto renderScene = [&]() 
+	auto renderScene = [&]()
 	{
 		rect16 startDot = rect16(point16(play_again_text_rect.left(), (play_again_text_rect.y1 + play_again_text_rect.y2) / 2).offset(-10, -6), size16(6, 6));
 		rect16 exitDot = rect16(point16(exit_text_rect.left(), (exit_text_rect.y1 + exit_text_rect.y2) / 2).offset(-10, -6), size16(6, 6));
 		switch (selectedButton)
 		{
-		case 0: {
+		case 0:
+		{
 			lcd.clear(exitDot);
 			draw::filled_ellipse(lcd, startDot, color<pixel_type>::white);
 			break;
@@ -222,7 +197,7 @@ Main::GameState Main::runEndScreen()
 	renderScene();
 
 	while (true)
-	{		
+	{
 		controller.capture();
 
 		if (controller.getButtonState(BUTTON_UP))
@@ -252,14 +227,14 @@ Main::GameState Main::runEndScreen()
 			case 1:
 				exitState = GameState::Exit;
 			}
-			
+
 			break;
 		}
 	}
 
 	lcd.clear(lcd.bounds());
 
-	return exitState;	
+	return exitState;
 }
 
 point16 _drawJPEG_destination;
